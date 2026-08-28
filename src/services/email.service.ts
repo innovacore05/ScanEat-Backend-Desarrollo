d6a669139da9d9f33c381a032644d406024ef4e9
LauraArroyo10 <la.arroyo.rivera@gmail.com>
2026-08-27 21:42:38 -0600

    tiempo max carga

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -12,6 +12,9 @@ function createTransporter() {
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT || 587),
     secure: String(process.env.SMTP_SECURE).toLowerCase() === 'true',
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASSWORD,
