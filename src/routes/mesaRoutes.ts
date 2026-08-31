8e29b21de36c15613e729a0d783fe3315ee4185c
LauraArroyo10 <la.arroyo.rivera@gmail.com>
2026-08-30 23:10:01 -0600

    actualizacion de ali al hosting

--- src/routes/mesaRoutes.ts
+++ src/routes/mesaRoutes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { createTable, getTables, getTableById } from "../controllers/mesaController";
 import { validateBody, validateParams } from "../middleware/validations";
 import { createTableSchema, tableParamsSchema } from "../db/schemas/mesaSchema";
-
+//actualziacion de ali
 const router = Router();
 
 router.post("/", validateBody(createTableSchema), createTable);
